e5d5018e96afe1315a933fa796e30a053d2aa786
Roger Garcia <roger@expertte.com>
2014-01-26 00:57:29 -0200

    Added mana source. Must refactor\!

--- card/mana_cost.rb
+++ card/mana_cost.rb
@@ -7,6 +7,24 @@ module ManaCost
   # G: green
   MANA_TYPES = [:C, :W, :U, :B, :R, :G]
 
+  # TODO: REFACTOR!!
+  def mana_source(*args)
+    unless args.empty?
+      @mana_source ||= {}
+      args.each do |arg|
+        if arg.is_a? Fixnum
+          @mana_source[:C] = arg
+        elsif arg.is_a? Symbol
+          @mana_source[arg] ||= 0
+          @mana_source[arg] += 1
+        else
+          throw 'Mana type not recognized'
+        end
+      end
+    end
+    @mana_source
+  end
+
   def mana_cost(*args)
     unless args.empty?
       @mana_cost ||= {}
